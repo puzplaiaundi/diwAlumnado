eef9a6d79918b258b85fc650b54f59666f5e5882
puzquiano3 <pauzsk8@gmail.com>
2026-01-12 12:55:03 +0100

    frameworks

---
+++ Accesibilidad/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e1e1fff',
+        secondary: '#9333EA',
+        success: '#16A34A',
+        danger: '#DC2626',
+        surface: '#F8FAFC',
+        'on-surface': '#0F172A'
+      }
+    }
+  }
+}
+
